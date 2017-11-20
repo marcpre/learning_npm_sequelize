1f30707f56869553f0a2f624c81d371e8bca959c
marcpre <marcus.presich@gmail.com>
2017-11-20 05:51:11 +0000

    adding model

---
+++ src/services/t02-Model.js
@@ -0,0 +1,20 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../db/db.js')
+
+const User = sequelize.define('user', {
+  firstName: {
+    type: Sequelize.STRING
+  },
+  lastName: {
+    type: Sequelize.STRING
+  }
+});
+
+// force: true will drop the table if it already exists
+User.sync({force: true}).then(() => {
+  // Table created
+  return User.create({
+    firstName: 'John',
+    lastName: 'Hancock'
+  });
+});
