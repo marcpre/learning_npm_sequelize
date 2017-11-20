3a7e442b94aebe2bcaf83609b29cb05f7b440e9a
marcpre <marcus.presich@gmail.com>
2017-11-20 05:35:40 +0000

    init

---
+++ src/db/db.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: 'localhost',
+  dialect: 'postgres',
+})
+
+module.exports = sequelize
