5902c189669f117722e164402a1da296599b9c0b
marcpre <marcus.presich@gmail.com>
2019-07-20 14:06:15 +0200

    fixing companyID to dividend

--- test/DividendServiceTest.js
+++ test/DividendServiceTest.js
@@ -1,16 +1,16 @@
 const chai = require('chai')
 chai.use(require('chai-datetime'))
+
 const assert = chai.assert
 
 const models = require('../src/models')
 
 const factories = require('./factories')
 const truncate = require('./truncate')
-// const { DividendService }  = require('../Services/DividendService')
-
 
 describe('Dividend model', () => {
   let dividend
+  let company
 
   beforeEach(async () => {
     // setup db
@@ -19,11 +19,16 @@ describe('Dividend model', () => {
     await truncate()
 
     // add 1 factory to the db
-    dividend = await factories.factories.dividend()
+    company = await factories.factories.company()
+    dividend = await factories.factories.dividend(company)
   })
 
   it('should generate a dividend from the factory', async () => {
     assert.isOk(dividend.id)
+  })
+  
+  it('company object should have the same id in the dividends table', async () => {
+    assert.equal(dividend.CompanyId, company.id)
   })
 
   it('should truncate the dividend table with each test', async () => {
@@ -36,7 +41,7 @@ describe('Dividend model', () => {
     const numberOfDividends = 10
 
     for (let i = 0; i < numberOfDividends; i++) {
-      await factories.factories.dividend()
+      await factories.factories.dividend(company)
     }
 
     const count = await models.Dividend.count()
